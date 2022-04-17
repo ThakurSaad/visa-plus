4218975836ba30e261f51b05a53c6d15e8aabf58
thakurSaad <thakursaad613@gmail.com>
2022-04-17 23:21:20 +0600

    homepage responsive done

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -17,7 +17,7 @@ const Services = () => {
       >
         My Services
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
         {services.map((service) => (
           <Service key={service.id} service={service}></Service>
         ))}
