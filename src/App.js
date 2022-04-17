ccfad0d0449c54c1936d0de3ef43d3a9453444fd
thakurSaad <thakursaad613@gmail.com>
2022-04-17 23:07:28 +0600

    404 Route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from "./components/Home/Home/Home";
 import Login from "./components/Login/Login/Login";
 import Register from "./components/Login/Register/Register";
 import RequireAuth from "./components/Login/RequireAuth/RequireAuth";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
             </RequireAuth>
           }
         ></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="mt-40 mb-96">
+      <div className="text-center text-6xl font-semibold text-sky-900">
+        <p>The page you are looking for does not exist</p>
+        <p>Error code 404 !</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
