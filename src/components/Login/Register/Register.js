977eb1de4ac17ace1f3b77908427c973d11e6145
thakurSaad <thakursaad613@gmail.com>
2022-04-17 13:12:09 +0600

    register form basic functionality

--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -1,18 +1,58 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  let errorElement;
+  let displayLoading;
+
+  if (user) {
+    navigate("/");
+  }
+  if (loading) {
+    displayLoading = (
+      <p className="text-sm text-center rounded-md bg-sky-900 text-white absolute left-0 right-0 top-20 mx-auto w-72 p-2">
+        Loading...
+      </p>
+    );
+  }
+  if (error) {
+    errorElement = (
+      <p className="text-sm rounded-md bg-orange-100 text-red-700 absolute left-0 right-0 my-3 mx-auto w-72 p-2">
+        {error.message}
+      </p>
+    );
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const confirmPassword = event.target.confirmPassword.value;
+    console.log(name, email, password, confirmPassword);
+    if (password === confirmPassword) {
+      createUserWithEmailAndPassword(email, password);
+    } else {
+      alert("type confirm password carefully");
+    }
+  };
   return (
     <div>
       <section className="mt-20" id="register-form">
-        {/* {displayLoading} */}
-        <form>
+        {displayLoading}
+        <form onSubmit={handleSubmit}>
           <input
             className="text-sm border-none rounded-md bg-gray-100 block my-3 mx-auto w-72 py-4 pl-3"
             placeholder="Your Name"
             type="text"
             name="name"
             id="name-register"
+            required
           />
           <input
             className="text-sm border-none rounded-md bg-gray-100 block my-3 mx-auto w-72 py-4 pl-3"
@@ -20,6 +60,7 @@ const Register = () => {
             type="email"
             name="email"
             id="email-register"
+            required
           />
           <input
             className="text-sm border-none rounded-md bg-gray-100 block my-3 mx-auto w-72 py-4 pl-3"
@@ -27,6 +68,7 @@ const Register = () => {
             type="password"
             name="password"
             id="password-register"
+            required
           />
           <input
             className="text-sm border-none rounded-md bg-gray-100 block my-3 mx-auto w-72 py-4 pl-3"
@@ -34,6 +76,7 @@ const Register = () => {
             type="password"
             name="confirmPassword"
             id="confirm-password-register"
+            required
           />
           <input
             className="text-sm border-none rounded-md text-white bg-sky-900 my-1 mx-auto block w-72 py-4"
@@ -47,7 +90,7 @@ const Register = () => {
             Login
           </Link>
         </p>
-        {/* {errorElement} */}
+        {errorElement}
       </section>
     </div>
   );
